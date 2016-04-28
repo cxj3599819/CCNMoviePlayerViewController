43794b238c694ff02654795692bda8570da76f0d
cxj3599819 <zcc@zccdemacbook-pro.local>
2016-04-28 12:22:21 +0800

    Initial Commit

---
+++ CCNMoviePlayerViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCNMoviePlayerViewController
+//
+//  Created by zcc on 16/4/28.
+//  Copyright © 2016年 CCN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCNMoviePlayerViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCNMoviePlayerViewController
+//
+//  Created by zcc on 16/4/28.
+//  Copyright © 2016年 CCN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
